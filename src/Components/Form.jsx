06d18e32285a21b8f73a906b7890e252f98427c9
Andrea Latuff <andrealatuff@Gregorios-MacBook-Air.local>
2023-06-30 11:21:52 -0500

    correccion form y estilos

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,34 +1,60 @@
-
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 const Form = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [error, setError] = useState('');
-  const [successMessage, setSuccessMessage] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-    if (name.length < 5 && !email.includes('@')) {
-      setError('Error!! Chequea nuevamente la info');
+
+    if (name.length < 5) {
+      setError("Error!! Chequea nuevamente la info");
+      setTimeout(() => {
+        setError('');
+      }, 4000);
+      return;
+    } else if (!email.includes("@")) {
+      setError("Error!! Chequea nuevamente la info");
+      setTimeout(() => {
+        setError('');
+      }, 4000);
       return;
     }
-
     setSuccessMessage(`Genial ${name}, ya te enviamos un mail`);
+    setError('');
   };
+
+  useEffect(() => {
+    if (successMessage) {
+      const mssg = setTimeout(() => {
+        setSuccessMessage("");
+      }, 4000);
+      return () => clearTimeout(mssg);
+    }
+  }, [successMessage]);
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <label>
           Nombre completo:
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </label>
         <br />
         <label>
           Email:
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <br />
         <button type="submit">Enviar</button>
